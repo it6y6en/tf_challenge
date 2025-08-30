4ec1c3af9ddd4f589f3291b8dcbb6232bd0387ba
Aleksei Kostenko <23655380+it6y6en@users.noreply.github.com>
2025-08-29 20:01:28 -0700

    internet route for public subnets

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -34,3 +34,34 @@ resource "aws_subnet" "private" {
     Name = "ansys-interview-${each.key}"
   }
 }
+
+resource "aws_internet_gateway" "this" {
+  tags = {
+    Name = "ansys-interview-ig"
+  }
+}
+
+resource "aws_internet_gateway_attachment" "this" {
+  internet_gateway_id = aws_internet_gateway.this.id
+  vpc_id              = aws_vpc.this.id
+}
+
+resource "aws_route_table" "this" {
+  vpc_id = aws_vpc.this.id
+
+  tags = {
+    Name = "ansys-interview-rt"
+  }
+}
+
+resource "aws_route" "this" {
+  route_table_id         = aws_route_table.this.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.this.id
+}
+
+resource "aws_route_table_association" "this" {
+  for_each       = aws_subnet.public
+  subnet_id      = aws_subnet.public[each.key].id
+  route_table_id = aws_route_table.this.id
+}
