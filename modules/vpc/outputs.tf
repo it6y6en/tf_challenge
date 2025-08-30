cd1dcf37d71780d7b402a49a74e3ce347d9cc714
Aleksei Kostenko <23655380+it6y6en@users.noreply.github.com>
2025-08-29 20:06:50 -0700

    subnets id's outputs

--- modules/vpc/outputs.tf
+++ modules/vpc/outputs.tf
@@ -1,3 +1,11 @@
 output "vpc_id" {
   value = aws_vpc.this.id
 }
+
+output "public_subnets" {
+  value = [for i in aws_subnet.public : i.id]
+}
+
+output "private_subnets" {
+  value = [for i in aws_subnet.private : i.id]
+}
